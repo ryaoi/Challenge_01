3f5079a6e290537f531fd78be0174160ce5a567a
ryaoi <ryaoi@student.42.fr>
2016-12-08 13:40:02 +0100

    Update challenge.c
    un peu lent

--- challenge.c
+++ challenge.c
@@ -12,11 +12,6 @@
 
 #include "challenge.h"
 
-// peut mieux opstimiser les ft_select_min et ft_select_max
-// try to use math.c
-// or try to find an equation
-// ...c'est trop moche mon dieux
-
 unsigned long long int  ft_select_min(unsigned long long int size, unsigned long long int triple, unsigned long long int average)
 {
     if (size <= 101)
@@ -60,22 +55,20 @@ unsigned long long int  ft_select_max(unsigned long long int size, unsigned long
 t_cubic		*init(unsigned long long int triple, unsigned long long int average, unsigned long long int size, unsigned long long int max_size)
 {
     t_cubic *new;
-    new = (t_cubic *)malloc(sizeof(t_cubic)); //sans malloc = no life
+    new = (t_cubic *)malloc(sizeof(t_cubic));
     new->x = 1;
     new->y = 1;
     new->z = 1;
-    new->min = ft_select_min(size, triple, average); // pour eviter que ca fait une boucle de 1 a 10Million
-    new->max = ft_select_max(size, triple, average); // raison pareil
-    new->result = ULLONG_MAX;  //bon peut etre que c'est trop....
-    new->limit = max_size; //ca ne doit pas depasser le racine cubique ^ 3 sinon c bizare...
-    new->size = size; //permet de comparer la taille de xyz pendant le boucle
-    new->reach = triple + 1;  //sachant que le cube(xyz) qui a le plus petite surface est entre triple - 1 et triple + 1,
-    new->average = average; //la moyenne pour qu'on ne lance pas une boucle avec un int qui va de 1 a 10million.
-    new->triple = triple; //la partie qui va permettre de stop le backtrack en creant une limite qui est reach
+    new->min = ft_select_min(size, triple, average);
+    new->max = ft_select_max(size, triple, average);
+    new->result = ULLONG_MAX;
+    new->limit = max_size;
+    new->size = size;
+    new->reach = triple + 1;
+    new->average = average;
+    new->triple = triple;
     return (new);
 }
-
-//utilisation de backtrack mais on peut faire ameliorer
 
 unsigned long long int  solve(t_cubic **cube, unsigned long long int triple)
 {
@@ -112,9 +105,6 @@ unsigned long long int  solve(t_cubic **cube, unsigned long long int triple)
     return (solve(cube, ++triple));
 }
 
-//la partie la plus importante
-//trouver la racine cubique
-
 unsigned long long int     find_xyz(unsigned long long int x, unsigned long long int size)
 {
     if(size == 1)
@@ -125,11 +115,6 @@ unsigned long long int     find_xyz(unsigned long long int x, unsigned long long
         return (x);
 }
 
-//pas de utlisation de atoi
-//sinon on peut creer un atoull
-//printf ralentie le programme, mieux utiliser putchar putnbr?
-
-
 int		main(void)
 {
     unsigned long long int result;
@@ -139,15 +124,15 @@ int		main(void)
     unsigned long long int cubic;
     t_cubic *cube;
     
-    size = 10000000001;
+    size = 10000000001;     /*----Modifier les sizes comme vous voulez----*/
     printf("size is:%llu\n", size);
     result = 0;
     cubic = find_xyz(1, size);
-    max_size = cubic * cubic * cubic; //creation de notre limit
-    triple = cubic + cubic + cubic; //cette valeur triple va nous permettre a trouver la solution car x + y + z doit etre infeiruer a cette valeur pour que la cube (xyz) contien la plus petite surface
-    cube = init(triple, cubic, size, max_size); //initialization
-    result = solve(&cube, triple - 1); //lance le backtrack;
+    max_size = cubic * cubic * cubic;
+    triple = cubic + cubic + cubic;
+    cube = init(triple, cubic, size, max_size);
+    result = solve(&cube, triple - 1);
     printf("result:%llu\n", result);
-    free(cube);  // pas de leak, pas de fuite de memoire
+    free(cube);
     return (0);
 }
