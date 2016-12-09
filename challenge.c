5ef4bf4c18755a6846119fd8786639124744d046
ryaoi <ryaoi@student.42.fr>
2016-12-09 01:03:13 +0100

    Update challenge.c

--- challenge.c
+++ challenge.c
@@ -32,6 +32,41 @@ unsigned long long int  ft_select_min(unsigned long long int size, unsigned long
         exit(EXIT_FAILURE);
 }
 
+/*
+Un petit indice pour les gens qui vont avoir une variable qui est au dessus de 64bits
+
+Utilisez unsigned __int128
+
+unsigned __int128 ft_select_min(unsigned __int128 size, unsigned __int128 triple, unsigned __int128 average)
+{
+    if (size <= 101)
+        return (1);
+    else if (size <= 10000001)
+        return ((triple / 3) - (average / 4));
+    else if (size <= 100000001)
+        return ((triple / 3) - (average / 10));
+    else if (size <= 100000000001)
+        return ((triple / 3) - (average / 20));
+    else if (size <= 1000000000001)
+        return ((triple / 3) - (average / 30));
+    else if (size <= 10000000000001)
+        return ((triple / 3) - (average / 125));
+    else if (size <= 1000000000000001)
+        return ((triple / 3) - (average / 275));
+    else if (size <= 10000000000000001) //start from 215444
+        return ((triple / 3) - (average / 400));
+    else if (size <= 100000000000000001) //start from 464159
+        return ((triple / 3) - (average / 500));
+    else if (size <= 1000000000000000001)  //start from 1000001
+        return ((triple / 3) - (average / 700));
+    else if (size <= ULLONG_MAX) //start from 2642246
+        return ((triple / 3) - (average / 2000));
+    else
+        exit(EXIT_FAILURE);
+}
+
+*/
+
 unsigned long long int  ft_select_max(unsigned long long int size, unsigned long long int triple, unsigned long long int average)
 {
     if (size <= 101)
@@ -61,7 +96,7 @@ t_cubic		*init(unsigned long long int triple, unsigned long long int average, un
     new->z = 1;
     new->min = ft_select_min(size, triple, average);
     new->max = ft_select_max(size, triple, average);
-    new->result = ULLONG_MAX;
+    new->result = ULLONG_MAX; // si vous voulez trouver des valeurs au dessus de ULLONG_MAX changer celle ci en ((unsinged __int128)UINT64_MAX + 1) * 0x1234567890ABCDEFULL + 0xFEDCBA9876543210ULL;
     new->limit = max_size;
     new->size = size;
     new->reach = triple + 1;
@@ -119,12 +154,14 @@ int		main(void)
     unsigned long long int max_size;
     unsigned long long int triple;
     unsigned long long int cubic;
+    unsigned long long int start;
     t_cubic *cube;
     
     size = 10000000001;     /*----Modifier les sizes comme vous voulez----*/
+    start = 1; //Changer cette valeur a partir de 10000000000000001 Sinon ca va segfault
     printf("size is:%llu\n", size);
     result = 0;
-    cubic = find_xyz(1, size);
+    cubic = find_xyz(start, size); //
     max_size = cubic * cubic * cubic;
     triple = cubic + cubic + cubic;
     cube = init(triple, cubic, size, max_size);
